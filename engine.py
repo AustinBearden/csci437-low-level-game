0bd70a59c48353f7f55894359c82615f774a4fb3
aibearde <aibearde@iupui.edu>
2018-11-24 15:43:53 -0500

    Got a sort of game loop together in the main section

--- engine.py
+++ engine.py
@@ -13,48 +13,81 @@ import sys, pygame
     # color : rgb for now, but later will be able to take standard color names like CSS can
 class MiniWorld():
 
-    def __init__(self):
+    def __init__(self, width, height):
         pygame.init()
-        self.mWScreen = pygame.display.set_mode((200, 100))
+        self.inputWidth = width
+        self.inputHeight = height
+        self.mWScreen = pygame.display.set_mode((self.inputWidth, self.inputHeight))
         self.mWScreen.fill((12, 0, 12))
+        self.go = True;
 
-    def setSize(self, width, height):
-        # how do I change the size of the screen ?
-        print("Oh yeah!")
+    # def setSize(self, width, height):
+    #     # how do I change the size of the screen ?
+    #     print("Oh yeah!")
 
-    def setColor(self, red, yellow, blue):
-        color = red, yellow, blue
-        self.mWScreen.fill(color)
+    # def setColor(self, red, yellow, blue):
+    #     color = red, yellow, blue
+    #     self.mWScreen.fill(color)
 
     def update(self):
         # Update everything that needs to be updated
-        pygame.display.flip()
+        print("Update method")
+
+    # start game play method
+    def start(self):
+        self.mWScreen = pygame.display.set_mode((self.inputWidth, self.inputHeight))
+        self.mWScreen.fill((12, 0, 12))
+
+
+
+    # end game play method
+    def end(self):
+        self.go = False
+
+        
 
     # change/set color
     # change size
 
 # end of MiniWorld class (basically the scene for the engine)
 
-# class for sprite
-class MiniThing(pygame.sprite.Sprite, image):
-    def __init__(self):
-        # initiate a pygame sprite
+# # class for sprite
+# class MiniThing(pygame.sprite.Sprite, image):
+#     def __init__(self):
+#         # initiate a pygame sprite
 
 
-    # change sprite's image
-    def setImage(image):
+
+#     # change sprite's image
+#     def setImage(image):
+#         # content of setImage() method
+
+#     # set sprite's angle
+#     def setAngle(angleDegrees):
+#         # content of setAngle() method
+
+#     # change sprite's angle by specified amount
+#     def changeAngle(angleDegrees):
+#         # content of changeAngle() method
+
+#     # set sprite's color + dimension (this only works if no image loaded)
+#     def setDimensionColor(color, width, height):
+#         # content of setDimensionColor() method
+
+#     # change the sprite's speed
+#     def setSpeed(speed):
+#         # content of setSpeed() method
+
+#     # set the sprite's position
+#     def setPosition(x, y):
+#         # content of setPosition() method
+
+    
+#     # change the sprite's position by certain x and certain y
+#     def changePosition(x, y):
+#         # content of changePosition() method
 
 
-    # set sprite's angle
-    def setAngle(angleDegrees):
-
-
-    # set sprite's color + dimension (this only works if no image loaded)
-    def setDimensionColor(color, width, height):
-
-
-    # 
-        
         
 
 
@@ -62,12 +95,20 @@ class MiniThing(pygame.sprite.Sprite, image):
     
 
 def main():
-    coolDude = MiniWorld()
-    i = 0
-    while(True):
-        coolDude.setSize(300, 500)
-        coolDude.setColor(0, 0, 0)
-        coolDude.update()
+    coolDude = MiniWorld(1000, 900)
+    while(coolDude.go):
+        coolDude.start()
+        i = 0
+        while(i < 30000000):
+            i = i + 1
+            print("hey man")
+            print("hey Jeremy")
+        
+        coolDude.end()
+
+
+
+
 
 if __name__ == "__main__":
     main()
