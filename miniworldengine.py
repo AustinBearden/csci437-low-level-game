77c7a7133681ce6addf149e6c691d9326987883e
aibearde <aibearde@iupui.edu>
2018-11-28 00:08:54 -0500

    Added basic game loop to mini-world-engine

--- miniworldengine.py
+++ miniworldengine.py
@@ -8,6 +8,7 @@ Assignment: csci437-low-level-game
 # Import pygame library of functions
 import sys, pygame
 
+# MiniWorld is the screen inn pygame
 # input params to MiniWorld instance:
     # width, height
     # color : rgb for now, but later will be able to take standard color names like CSS can
@@ -29,23 +30,10 @@ class MiniWorld():
     #     color = red, yellow, blue
     #     self.mWScreen.fill(color)
 
-    def update(self):
-        # Update everything that needs to be updated
-        print("Update method")
+    
 
-    # start game play method
-    def start(self):
-        self.mWScreen = pygame.display.set_mode((self.inputWidth, self.inputHeight))
-        self.mWScreen.fill((12, 0, 12))
-
-
-
-    # end game play method
-    def end(self):
-        self.go = False
 
         
-
     # change/set color
     # change size
 
@@ -88,25 +76,27 @@ class MiniWorld():
 #         # content of changePosition() method
 
 
-        
-
 
 # end of MiniThing class (basically the sprite class for the engine)
     
 
 def main():
     coolDude = MiniWorld(1000, 900)
-    while(coolDude.go):
-        coolDude.start()
-        i = 0
-        while(i < 30000000):
-            i = i + 1
-            print("hey man")
-            print("hey Jeremy")
-        
-        coolDude.end()
+    '''
+    Update loop:
+    All content in this while loop is the game loop section
+    Thanks to Pete Shinners to the code here: https://www.pygame.org/docs/tut/PygameIntro.html
+    '''
+    while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: sys.exit()
 
-
+            '''
+                ...empty game loop
+            '''
+            
+            coolDude.mWScreen.fill((240, 240, 255))
+            pygame.display.flip()
 
 
 
