fa1c0f7797844b1ac5029bab4af18e948f2bba31
aibearde <aibearde@iupui.edu>
2018-12-08 14:48:12 -0500

    With sprite groups

--- miniworldengine.py
+++ miniworldengine.py
@@ -29,17 +29,16 @@ class MiniThing(pygame.sprite.Sprite):
     # this will subclass Sprite
 
     def __init__(self, color, width, height):
-        Super(MiniThing, self).__init__()
+        
+        pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
         
         self.rect = self.image.get_rect()
 
-    def update(self):
-
-
     
+
 
 def main():
     coolDude = MiniWorld(1000, 900)
