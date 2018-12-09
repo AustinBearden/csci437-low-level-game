021cd4cf5c89c39c5941b8cb2130b90a0f679d9f
aibearde <aibearde@iupui.edu>
2018-12-08 20:57:15 -0500

    Successfully got a update method included. Now just need to refresh the background image

--- miniworldengine.py
+++ miniworldengine.py
@@ -6,7 +6,8 @@ Assignment: csci437-low-level-game
 '''
 
 # Import pygame library of functions
-import sys, pygame
+import sys
+import pygame
 
 # MiniWorld is the screen inn pygame
 # input params to MiniWorld instance:
@@ -19,52 +20,135 @@ class MiniWorld():
         self.inputWidth = width
         self.inputHeight = height
         self.mWScreen = pygame.display.set_mode((self.inputWidth, self.inputHeight))
+        pygame.display.set_caption('MiniWorld')
         self.mWScreen.fill((12, 0, 12))
         self.go = True
+        self.clock = pygame.time.Clock()
+        
 
 # sprite class
 # parameters:
     #
-class MiniThing(pygame.sprite.Sprite):
+class MiniThing(MiniWorld, pygame.sprite.Sprite):
     # this will subclass Sprite
 
-    def __init__(self, color, width, height):
+    def __init__(self, MiniWorld, color, width, height):
         
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
-        
+
         self.rect = self.image.get_rect()
+
+        self.x = 0
+        self.y = 0
+        self.dx = 0
+        self.dy = 0
+        self.theSurface = MiniWorld.mWScreen
+       
+        
+    # end of constructor method
+
+    # begining of helper and action methods
+    def getX(self):
+        return self.x
+    
+    def getY(self):
+        return self.y
+    
+    def getDX(self):
+        return self.dx
+
+    def getDY(self):
+        return self.dy
+
+    def setX(self, x):
+        self.x = x
+
+    def setY(self, y):
+        self.y = y
+    
+    def setDX(self, dx):
+        self.dx = dx
+    
+    def setDY(self, dy):
+        self.dy = dy
+
+    def changeXby(self, x):
+        self.x += x
+
+    def changeYby(self, y):
+        self.y += y
+
+    def changeDXby(self, dx):
+        self.dx += dx
+
+    def changeDYby(self, dy):
+        self.dy += dy
+    
+    # end of helper and action methods
+
+
+    # check and respond to key actions
+    #   change argument is to define amount changed for each key-press
+    # event is the event passed in
+    def keyActions(self, event, change):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print("Left", change)
+                self.changeXby(-change)
+            if event.key == pygame.K_RIGHT:
+                print("Right", change)
+                self.changeXby(change)
+            if event.key == pygame.K_UP:
+                print("Up", change)
+                self.changeYby(-change)
+            if event.key == pygame.K_DOWN:
+                print("Down", change)
+                self.changeYby(change)
+        
+        
+    # Update method
+    def update(self):
+        self.theSurface.blit(self.image, (self.x, self.y))
 
     
 
 
 def main():
     coolDude = MiniWorld(1000, 900)
-
     color = (255, 0, 0)
+    
     '''
     Update loop:
     All content in this while loop is the game loop section
     Thanks to Pete Shinners to the code here: https://www.pygame.org/docs/tut/PygameIntro.html
     '''
+    coolDude.mWScreen.fill((240, 240, 255))
+    coolThing = MiniThing(coolDude,color, 24, 24)
+
     while 1:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: sys.exit()
+                print(event)
 
             '''
                 ...empty game loop
             '''
 
-
-            coolThing = MiniThing(color, 24, 24)
-            coolGroup = pygame.sprite.Group(coolThing)
-
-            coolGroup.draw(coolDude.mWScreen);
             
-            coolDude.mWScreen.fill((240, 240, 255))
+            
+            
+            
+            #coolDude.update()
+    
+            coolThing.keyActions(event, 5)
+            coolThing.update()
             pygame.display.flip()
+            
+
+    pygame.quit()
 
 
 
